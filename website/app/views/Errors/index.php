8ac85d3af224b691a4dc84e2b2c4899c638e2e99
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2025-12-27 21:49:41 +0200

    update error page with the new schame for the website

--- website/app/views/Errors/index.php
+++ website/app/views/Errors/index.php
@@ -1,3 +1,13 @@
-<?php
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/app/templets/head.php'; ?>
+<body>
+<div class="container">
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/app/templets/header.php'; ?>
 
-echo 'Error ' . $data;
+    <div class="pagebody">
+        <article>
+            <h1>404! File Not Found</h1>
+        </article>
+    </div>
+</div>
+</body>
+</html>
