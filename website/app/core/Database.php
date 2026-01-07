7c7b2cab39d51bb067bdb6e0b9f0f3c4806b1db6
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2026-01-07 02:57:55 +0200

    refactor Database.php to improve connection handling, add query utilities, and use config.ini for credentials

--- website/app/core/Database.php
+++ website/app/core/Database.php
@@ -1,23 +1,93 @@
 <?php
 
+
+// I used https://github.com/Wagner-Souza/eloquent-orm/blob/main/orm/Database.php as a reference
+
+
+namespace Core;
+
+use PDO;
+use PDOException;
+use RuntimeException;
+use PDOStatement;
+
 class Database {
+    private static $config;
+    private static $connection;
 
-    private static $host = '<host>';
-    private static $db = '<database_name>';
-    private static $user = '<user>';
-    private static $pw = '<password>';
+    // check for connection
+    public static function getConnection(): PDO
+    {
+        if (self::$connection === null) {
+            if (empty(self::$config)) {
+                self::$config = parse_ini_file(__DIR__ . '/../config.ini');
+            }
+            self::connect();
+        }
 
-    private static function connect() {
-        $pdo = new PDO('mysql:host=' . self::$host . ';dbname=' . self::$db .';charset=utf8', self::$user, self::$pw);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        return $pdo;
+        return self::$connection;
     }
 
-    public static function query($query, $params = array()) {
-        $stmt = self::connect()->prepare($query);
-        $stmt->execute($params);
-        $data = $stmt->fetchAll();
-        return $data;
+    // connect to database
+    private static function connect(): void
+    {
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s;dbname=%s;charset=%s',
+            self::$config['dbhost'],
+            self::$config['port'],
+            self::$config['database'],
+            self::$config['charset']
+        );
+
+        try {
+            self::$connection = new PDO(
+                $dsn,
+                self::$config['username'],
+                self::$config['password'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                ]
+            );
+        } catch (PDOException $e) {
+            throw new RuntimeException("DB CONN: " . $e->getMessage());
+        }
+    }
+
+    // execute a query
+    private static function execute(string $sql, array $params = []): PDOStatement
+    {
+        $connection = self::getConnection();
+        $statement = $connection->prepare($sql);
+        $statement->execute($params);
+
+        return $statement;
+    }
+
+
+
+    // public functions
+    // getting all raws
+    public static function fetchAll(string $sql, array $params = []): array
+    {
+        return self::execute($sql, $params)->fetchAll();
+    }
+
+    // get one raw
+    public static function fetchOne(string $sql, array $params = []): array | bool
+    {
+        return self::execute($sql, $params)->fetch();
+    }
+
+    // last insert id
+    public static function lastInsertId(): string
+    {
+        return self::getConnection()->lastInsertId();
+    }
+
+    public static function disconnect(): void
+    {
+        self::$connection = null;
     }
 }
