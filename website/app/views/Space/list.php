262a2b8993af997a1f3c644352b1386f20d0d16c
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2026-01-08 21:09:39 +0200

    feat(styles, views): implement and style space list items
    - Add `.space-list-item` styles for consistent layout and hover effects.
    - Define new color variable `--color-slate-600` for text styling.
    - Update `Space/list.php` to display space list items with placeholders.
    - Remove unused CSS utility classes for streamlined styling.

--- website/app/views/Space/list.php
+++ website/app/views/Space/list.php
@@ -12,6 +12,43 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/app/templates/head.php';
             <h1>Discover Spaces!</h1>
             <div class="spaces-list">
                 AJAx would handle it
+                <div class="space-list-item">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkasanetetoplushies.com%2Fwp-content%2Fuploads%2F2025%2F03%2FKasane-Teto-Plush-2.png&f=1&nofb=1&ipt=0bcfa3a0a7fa59ded468a7b8095b085dd0e7dfd473949823c7fa188375fee92a"
+                         alt="space-icon">
+                    <div class="">
+                        <h2>Space name</h2>
+                        <p>Members: 324</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto asperiores
+                            dolore dolorum ducimus ea esse et eveniet in iure laboriosam, nisi, nulla perferendis,
+                            perspiciatis porro recusandae ullam voluptates voluptatibus!</p>
+                    </div>
+                    <button>Join</button>
+                </div>
+                <div class="space-list-item">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkasanetetoplushies.com%2Fwp-content%2Fuploads%2F2025%2F03%2FKasane-Teto-Plush-2.png&f=1&nofb=1&ipt=0bcfa3a0a7fa59ded468a7b8095b085dd0e7dfd473949823c7fa188375fee92a"
+                         alt="space-icon">
+                    <div class="">
+                        <h2>Space name</h2>
+                        <p>Members: 324</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto asperiores
+                            dolore dolorum ducimus ea esse et eveniet in iure laboriosam, nisi, nulla perferendis,
+                            perspiciatis porro recusandae ullam voluptates voluptatibus!</p>
+                    </div>
+                    <button>Join</button>
+                </div>
+
+                    <div class="space-list-item">
+                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkasanetetoplushies.com%2Fwp-content%2Fuploads%2F2025%2F03%2FKasane-Teto-Plush-2.png&f=1&nofb=1&ipt=0bcfa3a0a7fa59ded468a7b8095b085dd0e7dfd473949823c7fa188375fee92a"
+                             alt="space-icon">
+                        <div class="">
+                            <h2><a href="/Space/details/1">Space name</a></h2>
+                            <p>Members: 324</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto asperiores
+                                dolore dolorum ducimus ea esse et eveniet in iure laboriosam, nisi, nulla perferendis,
+                                perspiciatis porro recusandae ullam voluptates voluptatibus!</p>
+                        </div>
+                        <button>Join</button>
+                    </div>
             </div>
         </article>
     </div>
