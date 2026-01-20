bc821a153c3ab188e74bf0de9fb048330da9219b
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2026-01-20 22:28:45 +0200

    feat(uploads): add `Uploads` controller for handling image rendering
    - Add `jpeg` and `png` methods to serve images based on format.
    - Include basic 404 handling using `ErrorForward`.
    - Add sample images to `/uploads` folder (e.g., `avatar.jpg`, `cover.jpg`, `test.jpg`).

---
+++ website/app/controllers/Uploads.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace controllers;
+use core\Controller;
+
+class Uploads extends Controller {
+
+    // Index of the Home page (localhost/Home(/index))
+    public function jpeg($image) {
+        if (!isset($image)){
+            $this->ErrorForward();
+        }
+        $filename = $_SERVER['DOCUMENT_ROOT'] . "/uploads/" . $image . '.jpg';
+        $im = imagecreatefromjpeg( $filename );
+        if ($im === false){
+            var_dump($im);
+        }
+
+        header('Content-Type: image/jpeg');
+        imagejpeg($im);
+    }
+
+    public function png($image) {
+        if (!isset($image)){
+            $this->ErrorForward();
+        }
+        $filename = $_SERVER['DOCUMENT_ROOT'] . "/uploads/" . $image . '.png';
+        $im = imagecreatefrompng( $filename );
+        if ($im === false){
+            $this->ErrorForward();
+        }
+
+        header('Content-Type: image/png');
+        imagepng($im);
+    }
+
+}
