d1a5127c6f7307eafa427bdaef93cd359b8be976
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-02-03 01:49:35 +0200

    implement the new interfaces and traits update the headers to be correct
    update the functions in the model
    
    create the creation logic to create a new space with the needed validations

--- website/app/controllers/Space.php
+++ website/app/controllers/Space.php
@@ -4,11 +4,16 @@ namespace controllers;
 
 use core\Controller;
 use models\SpaceModel;
-use interfaces\CRUD;
+use interfaces\iController;
+use traits\CheckLogin;
+use traits\postRequestCheck;
+use core\Validation;
 
 class Space extends Controller
-implements CRUD
+implements iController
 {
+    use postRequestCheck;
+    use CheckLogin;
     protected $spaceModel;
     protected $spaces = [];
     protected $space = [];
@@ -18,9 +23,9 @@ implements CRUD
         $this->spaceModel = new SpaceModel();
     }
 
-    public function api($num, $for = ''){
-        if (!filter_var(FILTER_VALIDATE_INT ,$num) && !($num > 0)){
-            header('header: /ErrorPage/404');
+    public function api(int $num, string $for = ''){
+        if (!filter_var( $num, FILTER_VALIDATE_INT ) && !($num > 0)){
+            header('location: /ErrorPage/404');
         }
         switch($for){
             case '':
@@ -30,13 +35,13 @@ implements CRUD
                 $this->getAll($num);
                 break;
             default:
-                header('header: /ErrorPage/404');
+                header('location: /ErrorPage/404');
         }
     }
 
     public function index($num, $for = ''){
         if (!filter_var(FILTER_VALIDATE_INT ,$num) && !($num > 0)){
-            header('header: /ErrorPage/404');
+            header('location: /ErrorPage/404');
         }
         switch($for){
             case '':
@@ -50,10 +55,10 @@ implements CRUD
         }
     }
 
-    public function get($id)
+    public function get(int $id)
     {
         $data = [
-            'space' => $this->spaceModel->getSpace($id)[0],
+            'space' => $this->spaceModel->getByID($id)[0],
             'posts' => $this->spaceModel->getSpacePosts($id),
             'users_count' => $this->spaceModel->getUsersCount($id)[0]
         ];
@@ -61,11 +66,11 @@ implements CRUD
         echo json_encode($data);
     }
 
-    public function getAll($page = 1)
+    public function getAll(int $page = 1)
     {
         $page = ($page - 1) * 50;
         $data = [
-            'spaces' => $this->spaceModel->getSpaces($page),
+            'spaces' => $this->spaceModel->getAll($page),
         ];
         header('Content-Type: application/json');
         echo json_encode($data);
@@ -73,6 +78,52 @@ implements CRUD
 
     public function create(){
         // TODO
+        $this->postRequestCheck();
+        $this->checkLogin();
+
+        $category = isset($_POST['category_id']) ? (int)$_POST['category_id'] : null;
+        $name = $_POST['name'] ?? null;
+        $description = $_POST['description'] ?? null;
+        $csrf = $_POST['CSRFToken'] ?? null;
+        $captcha = $_POST['captcha'] ?? null;
+        $avatar = $_POST['avatar'] ?? null;
+        $cover = $_POST['cover'] ?? null;
+
+        $errors = new Validation();
+        $errors->checkCSRF($csrf);
+        $errors->checkCaptchaPhase($captcha);
+        $errors->checkMaxLength(["space_name" => [50, $name], "description" => [1000, $description]]);
+        $errors->checkMinLength(["space_name" => [2, $name]]);
+
+        if (!is_int($category)){
+            $errors->addError("category_id", "Category must be an integer");
+        }
+
+        if (!$this->spaceModel->checkCategory($category)){
+            $errors->addError("category", "Category doesn't exists");
+        }
+
+        if ($errors->getErrors()){
+            $_SESSION['errors'] = $errors->getErrors();
+            header('header: /Space/index/create');
+            die();
+        }
+
+        $values = [
+            'category_id' => $category,
+            'name' => $name,
+            'description' => $description ?? '',
+            'cover_image' => $cover ?? '',
+            'avatar' => $avatar ?? ''
+        ];
+
+        if (!$this->spaceModel->create($values)){
+            $errors->addError("error", "There was an error creating the space
+            , contact the administrator!");
+            $_SESSION['errors'] = $errors->getErrors();
+            header('header: /Space/index/create');
+            die();
+        }
     }
 
     public function update($id){
